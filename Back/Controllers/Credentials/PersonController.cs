13bfcba1f2682788147eff55c100077c832338fe
MauricioEnLaUni <210804@utags.edu.mx>
2023-02-24 00:48:49 -0600

    Dying I'm done

--- Back/Controllers/Credentials/PersonController.cs
+++ Back/Controllers/Credentials/PersonController.cs
@@ -71,21 +71,28 @@ namespace Fictichos.Constructora.Controller
             return charges;
         }
 
-        [HttpGet("{id}")]
-        public List<Job> GetJobsById(ObjectId personId)
+        [HttpGet("jobs/{id}")]
+        public List<Job> GetJobsById(string personId)
         {
             PersonConnector conn = new();
             List<Job> job =(List<Job>)
                 (from peeps in conn.Collection.AsQueryable()
-                where peeps.Id == personId
+                where peeps.Id == ObjectId.Parse(personId)
                 select peeps.Charges);
             return job;
         }
 
-        [HttpPut("update/{id}")]
-        public ActionResult UpdateJob(ObjectId personId, Job newJob)
+        [HttpPut("jobs/update")]
+        public ActionResult UpdateJob()
         {
             PersonConnector conn = new();
+            // Need to figure out how to post, fix later.
+            ObjectId personId = ObjectId.Parse("63f828a0cc4ca163fba421dc");
+            List<Job> newJob = new List<Job>();
+            newJob.Add(new Job("Barrendero","Employee","Intendencia"));
+            newJob.Add(new Job("Asesor","Admin","Gerencia"));
+            // Remove above block when possible.
+
             var filter = Builders<Person>.Filter.Eq("Id", personId);
             var update = Builders<Person>.Update.Set("Charges", newJob);
             conn.Collection.UpdateOne(filter, update);
