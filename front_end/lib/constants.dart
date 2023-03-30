f6f03dd264ace8dc79d692489ae7caa9105d8b92
EnriqueMTzEkO <enriquemartinez93893@gmail.com>
2023-03-30 13:17:33 -0600

    navbar incompleto

--- front_end/lib/constants.dart
+++ front_end/lib/constants.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/material.dart';
+
 abstract class Constants {
   static const String appUrl = String.fromEnvironment(
     'appUrl',
@@ -14,3 +16,7 @@ abstract class Constants {
     defaultValue: '',
   );
 }
+
+Color blueBar = Color.fromARGB(0, 99, 250, 248);
+Color shadow = Color.fromARGB(0, 24, 24, 24);
+Color pk = Color.fromARGB(255, 255, 191, 191);
