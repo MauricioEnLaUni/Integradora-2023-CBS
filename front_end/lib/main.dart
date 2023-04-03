f4293e6a1f9851140ac84c9da0b6c87b613881c8
MauricioEnLaUni <210804@utags.edu.mx>
2023-04-03 15:19:24 -0600

    User Dto Added a model class for users in the front end.

--- front_end/lib/main.dart
+++ front_end/lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:front_end/src/controllers/project_controller.dart';
+import 'package:front_end/src/models/project_model.dart';
 import './src/app.dart';
 
 void main() async {
+  List<ProjectDto> data = await ProjectController.getProjects();
+  print(data[0].starts);
   runApp(const FictichosBuilderCRM());
 }
 
---
+++ front_end/lib/src/models/user_info.dart
@@ -0,0 +1,27 @@
+import 'dart:convert';
+import 'dart:typed_data';
+import 'package:intl/intl.dart';
+import 'package:json_annotation/json_annotation.dart';
+
+import 'ftasks_model.dart';
+
+@JsonSerializable()
+class UserInfoDto {
+  UserInfoDto({ required this.name, required this.createdAt, required this.roles, required this.email, this.avatar });
+
+  String name;
+  DateTime createdAt;
+  List<String> roles;
+  List<String> email;
+  Uint8List? avatar;
+
+  factory UserInfoDto.fromJson(Map<String, dynamic> jsonString) {
+    return UserInfoDto(
+      name: jsonString['name'],
+      createdAt: DateFormat('yyyy-MM-dd').parse(jsonString['createdAt']),
+      roles: List<String>.from(jsonString['roles']),
+      email: List<String>.from(jsonString['email']),
+      avatar: jsonString['avatar'] != null ? Uint8List.fromList(jsonString['avatar']) : null
+    );
+  }
+}
